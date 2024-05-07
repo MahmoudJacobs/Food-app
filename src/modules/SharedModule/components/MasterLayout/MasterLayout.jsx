cdf99670fd79642cf20fe1db7308ca11bf9f61f3
MahmoudJacobs <156816329+MahmoudJacobs@users.noreply.github.com>
2024-05-08 01:47:08 +0300

    Update MasterLayout.jsx

--- src/modules/SharedModule/components/MasterLayout/MasterLayout.jsx
+++ src/modules/SharedModule/components/MasterLayout/MasterLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
-import SideBar from '../SideBar/SideBar'
+import SideBar from '../SideBar/Sidebar'
 import { Outlet } from "react-router-dom"
 
 
